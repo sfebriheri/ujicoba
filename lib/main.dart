dccf7b4868cb72a6301d7535e740ed60e83f05d0
sfebriheri <stywanheri@gmail.com>
2023-05-11 10:43:36 +0700

    testing-2

--- lib/main.dart
+++ lib/main.dart
@@ -59,29 +59,88 @@ class BigCard extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                  icon: Icon(Icons.favorite), label: Text('Favorites'),
+                  )
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: GeneratorPage(),
+           ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    
+    return Center
+    (child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BigCard(pair: pair),
+        SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            ElevatedButton(
+            ElevatedButton.icon(
               onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
-            ),
-          ],
-        ),
+                appState.toggleFavorite();
+              }, 
+              icon: Icon(icon), 
+              label:Text('Like'),
+              ),
+              SizedBox(width:10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                }, 
+                child: Text('Next'),
+                 ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
 
